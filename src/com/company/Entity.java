8ead181898ec71eb4efffff9a756c3d051bc7518
Paul Tataru <tataru.paul22@gmail.com>
2020-12-19 10:11:24 +0200

    I have add the method setters and getters in Entity class

--- src/com/company/Entity.java
+++ src/com/company/Entity.java
@@ -9,4 +9,38 @@ public class Entity {
     public int age;
 
     // TODO Getters and setters
+
+
+    public void setId(int id) {
+        this.id = id;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setClasses(List<Materie> classes) {
+        this.classes = classes;
+    }
+
+    public List<Materie> getClasses() {
+        return classes;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
